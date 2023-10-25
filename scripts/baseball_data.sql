c3b6d3e626636c7bc5db330b186064c8a0f3da8c
marthahunter <mgoodemotehunter@gmail.com>
2023-10-24 21:04:01 -0400

    working on q4

--- scripts/baseball_data.sql
+++ scripts/baseball_data.sql
@@ -51,6 +51,28 @@ LIMIT 1;
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
+SELECT 
+playerid,
+schoolname, 
+namefirst,
+namelast,
+SUM(salary) AS salary_sum
+FROM schools
+LEFT JOIN collegeplaying
+USING (schoolid)
+LEFT JOIN people
+USING (playerid)
+LEFT JOIN salaries
+USING (playerid)
+LEFT JOIN managers
+USING (playerid)
+WHERE schoolname LIKE '%Vanderbilt University%'
+And salary IS NOT NULL
+GROUP BY playerid, schoolname, namefirst, namelast
+ORDER BY salary_sum DESC
+
+-- 3A: David Price
+
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
